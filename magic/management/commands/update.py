9649a588b74895e089a81f11141ec80f6458aa19
Taylor Fausak <taylor@fausak.me>
2012-03-06 23:51:22 -0600

    Change comments to docstrings.

--- magic/management/commands/update.py
+++ magic/management/commands/update.py
@@ -1,3 +1,5 @@
+"""Update cards with information from the Gatherer.
+"""
 from BeautifulSoup import BeautifulSoup
 from django.core.management.base import BaseCommand
 from django.db import transaction
@@ -8,21 +10,25 @@ import re
 
 
 class Command(BaseCommand):
-    args = '<set set ...>'
-    help = 'Updates the specified sets'
+    args = '[set set ...]'
+    help = 'Update cards from the given sets.'
 
-    # A shortcut for printing stuff if the verbosity is high enough
-    def p(self, string, flush=False):
+    def write(self, string, flush=False):
+        """Shortcut for printing things if the verbosity is high enough.
+        """
         if self.verbosity:
             self.stdout.write(string)
             if flush:
                 self.stdout.flush()
 
-    # Switches from a list to a dict and does some rudimentary normalization
     def normalize_card(self, card):
+        """Switch from a list to a dict.
+
+        Also performs some rudimentary normalization.
+        """
         whitespace = re.compile(r'\s+')
 
-        # Switch from positional to named
+        # Switch from positional to named.
         card = {
             'name': card[0].contents[3].contents[1],
             'mana_cost': card[1].contents[3],
@@ -32,13 +38,13 @@ class Command(BaseCommand):
             'set_rarity': card[5].contents[3],
         }
 
-        # These fields are easy to normalize
+        # These fields are easy to normalize.
         for key in ('name', 'mana_cost', 'type', 'misc', 'set_rarity'):
             card[key] = card[key].string.strip().lower()
             card[key] = re.sub(whitespace, ' ', card[key])
             card[key] = re.sub(u'\u2019', "'", card[key])
 
-        # The rules text is a little more complex
+        # The rules text is a little more complex.
         rules_text = ''
         for rule in card['rules_text']:
             if rule.string:
@@ -49,28 +55,30 @@ class Command(BaseCommand):
 
         return card
 
-    # Converts a mana cost string to a list of string tokens
     def tokenize_mana_cost(self, mana_cost):
-        # Find and remove all hybrid mana costs
+        """Convert a mana cost string into a list of string tokens.
+        """
+        # Find and remove all hybrid mana costs.
         hybrid = re.findall(self.hybrid_pattern, mana_cost)
         mana_cost = re.sub(self.hybrid_pattern, '', mana_cost)
 
-        # Everthing else is just a regular mana cost
+        # Everthing else is just a regular mana cost.
         regular = re.findall(self.regular_pattern, mana_cost)
         return regular + hybrid
 
-    # Converts a tokenized mana cost into it's converted value
     def convert_mana_cost(self, mana_cost):
+        """Calculate the converted mana cost of a tokenized mana cost.
+        """
         converted_mana_cost = 0
 
         for token in mana_cost:
-            # If it's not a string (unicode), it's a tuple of strings
+            # If it's not a string (unicode), it's a tuple of strings.
             if not isinstance(token, unicode):
-                # The CMC of a hybrid is the max of the parts
+                # The CMC of a hybrid is the max of the parts.
                 converted_mana_cost += max([self.convert_mana_cost(x)
                     for x in token])
             else:
-                # Add the int value, 1 (for wubrg), or 0 (for xyz)
+                # Add the int value, 1 (for wubrg), or 0 (for xyz).
                 try:
                     converted_mana_cost += int(token)
                 except ValueError:
@@ -78,27 +86,30 @@ class Command(BaseCommand):
 
         return converted_mana_cost
 
-    # Converts a tokenized mana cost into it's string representation
     def stringify_mana_cost(self, mana_cost):
-        # Convert hybrid tuples into /-separated strings
+        """Convert a tokenized mana cost into a string.
+        """
+        # Convert hybrid tuples into /-separated strings.
         for index, token in enumerate(mana_cost):
             if not isinstance(token, str):
                 mana_cost[index] = '/'.join(token)
 
-        # Wrap everything in parentheses
+        # Wrap everything in parentheses.
         if not mana_cost:
             return ''
         return '(' + ')('.join(mana_cost) + ')'
 
-    # Returns the colors represented in a mana cost
     def colors_from_mana_cost(self, mana_cost):
+        """Figure out which colors are present in a mana cost.
+        """
         return [color for color in self.colors if color.slug in mana_cost]
 
-    # Extracts a card's super types from its type line
     def super_types_from_type(self, type_):
+        """Determine a card's super types from its type.
+        """
         types = []
 
-        # We only care about the left side of the type line
+        # We only care about the left side of the type line.
         type_ = type_.split(u'\u2014')[0].split()
         for super_type in self.super_types:
             if super_type.name in type_:
@@ -106,11 +117,12 @@ class Command(BaseCommand):
 
         return types
 
-    # Extracts a card's card types from its type line
     def card_types_from_type(self, type_):
+        """Determine a card's card type from its type.
+        """
         types = []
 
-        # We only care about the left side of the type line
+        # We only care about the left side of the type line.
         type_ = type_.split(u'\u2014')[0].split()
         for card_type in self.card_types:
             if card_type.name in type_:
@@ -118,16 +130,17 @@ class Command(BaseCommand):
 
         return types
 
-    # Extracts a card's sub types from its type line
     def sub_types_from_type(self, type_):
+        """Determine a card's sub type from its type.
+        """
         types = []
 
-        # We only care about the right side of the type line
+        # We only care about the right side of the type line.
         type_ = type_.split(u'\u2014')
         type_ = type_[1].split() if len(type_) > 1 else []
 
         for sub_type in self.sub_types:
-            # Some sub types have spaces in them
+            # Some sub types have spaces in them.
             if ' ' in sub_type.name:
                 append = sub_type.name in ' '.join(type_)
             else:
@@ -138,25 +151,29 @@ class Command(BaseCommand):
 
         return types
 
-    # Makes sense of the "miscellaneous" field (power, toughness, etc.)
     def resolve_misc(self, card):
+        """Make sense of the "miscellaneous" field.
+
+        It stands for power/toughness in creatures, loyalty in
+        plainswalkers, or hand/life modifiers in Vanguard cards.
+        """
         non_digit = re.compile('\D')
 
-        # Set defaults
+        # Set defaults.
         for key in ('power', 'toughness'):
             card[key] = ''
         for key in ('converted_power', 'converted_toughness', 'loyalty',
-            'hand_modifier', 'life_modifier'):
+                'hand_modifier', 'life_modifier'):
             card[key] = 0
 
-        # Pre-emptively grab P/T or H/L
+        # Pre-emptively grab P/T or H/L.
         matches = re.search(self.hybrid_pattern, card['misc'])
         if matches is None:
             left = right = '0'
         else:
             left, right = matches.groups('0')
 
-        # Conditionally set stuff based on card type
+        # Conditionally set stuff based on card type.
         if self.creature_type in card['card_types']:
             card['power'], card['toughness'] = left, right
             left = re.sub(non_digit, '', left) or 0
@@ -171,8 +188,9 @@ class Command(BaseCommand):
 
         del(card['misc'])
 
-    # Splits a comma-separated string into a list of Set/Rarity objects
     def split_set_rarity(self, set_rarity):
+        """Split a comma-separated string into a list of Set/Rarity objects.
+        """
         sets_rarities = []
 
         for set_rarity in set_rarity.split(', '):
@@ -187,10 +205,9 @@ class Command(BaseCommand):
 
         return sets_rarities
 
-    # The meat of the script
     @transaction.commit_manually
-    def handle(self, *args, **kwargs):
-        # Get everything set up to avoid DB calls and RE compiles
+    def handle(self, *args, **options):
+        # Get everything set up to avoid DB calls and RE compiles.
         self.card_types = magic.models.CardType.objects.all()
         self.colors = magic.models.Color.objects.all()
         self.creature_type = magic.models.CardType.objects.get(name='creature')
@@ -204,63 +221,63 @@ class Command(BaseCommand):
         self.super_types = magic.models.SuperType.objects.all()
         self.symbols = {'w': 1, 'u': 1, 'b': 1, 'r': 1, 'g': 1}
         self.vanguard_type = magic.models.CardType.objects.get(name='vanguard')
-        self.verbosity = int(kwargs['verbosity']) > 0
+        self.verbosity = int(options['verbosity']) > 0
 
-        # Build the Gatherer URL
+        # Build the Gatherer URL.
         url = 'http://gatherer.wizards.com/Pages/Search/Default.aspx' \
               '?method=text&output=spoiler&sort=abc+&special=true&set='
         if args:
             url += ''.join(['|["{0}"]'.format(quote(set)) for set in args])
         else:
             url += '|[]'
-        self.p(url + '\n')
+        self.write(url + '\n')
 
-        # Open and parse the page
-        self.p('Opening...\n')
+        # Open and parse the page.
+        self.write('Opening...\n')
         page = urlopen(url)
-        self.p('Parsing...\n')
+        self.write('Parsing...\n')
         soup = BeautifulSoup(page)
 
-        # Find cards
-        self.p('Finding cards...\n')
+        # Find cards.
+        self.write('Finding cards...\n')
         tags = soup.find('div', attrs={'class': 'textspoiler'})
         tags = tags.findAll('tr')
         cards = []
         for index in range(0, len(tags), 7):
             cards.append(tags[index:index + 6])
 
-        # Normalize cards
-        self.p('Normalizing...\n')
+        # Normalize cards.
+        self.write('Normalizing...\n')
         for index, card in enumerate(cards):
             cards[index] = self.normalize_card(card)
 
-        # Convert and calulcate mana cost
-        self.p('converted_mana_cost, mana_cost...\n')
+        # Convert and calulcate mana cost.
+        self.write('converted_mana_cost, mana_cost...\n')
         for card in cards:
             tokens = self.tokenize_mana_cost(card['mana_cost'])
             card['converted_mana_cost'] = self.convert_mana_cost(tokens)
             card['mana_cost'] = self.stringify_mana_cost(tokens)
 
-        # Determine colors
-        self.p('colors...\n')
+        # Determine colors.
+        self.write('colors...\n')
         for card in cards:
             card['colors'] = self.colors_from_mana_cost(card['mana_cost'])
 
-        # Determine types
-        self.p('super_types, card_types, sub_types...\n')
+        # Determine types.
+        self.write('super_types, card_types, sub_types...\n')
         for card in cards:
             card['super_types'] = self.super_types_from_type(card['type'])
             card['card_types'] = self.card_types_from_type(card['type'])
             card['sub_types'] = self.sub_types_from_type(card['type'])
             del(card['type'])
 
-        # Analyze miscellaneous field
-        self.p('misc...\n')
+        # Analyze miscellaneous field.
+        self.write('misc...\n')
         for card in cards:
             card = self.resolve_misc(card)
 
-        # Determine sets and rarities
-        self.p('set_rarity...\n')
+        # Determine sets and rarities.
+        self.write('set_rarity...\n')
         for rarity in self.rarities:
             rarity.re = re.compile(r' {0}$'.format(rarity.name))
         for card in cards:
@@ -268,8 +285,8 @@ class Command(BaseCommand):
                 card['set_rarity'])
             del(card['set_rarity'])
 
-        # Find the card IDs
-        self.p('id...\n')
+        # Find the card IDs.
+        self.write('id...\n')
         for card in cards:
             try:
                 card_db = magic.models.Card.objects.get(name=card['name'])
@@ -279,8 +296,8 @@ class Command(BaseCommand):
                 card['id'] = None
         transaction.commit()
 
-        # Save the cards
-        self.p('Saving...\n')
+        # Save the cards.
+        self.write('Saving...\n')
         for card in cards:
             card['db'] = magic.models.Card(
                 id=card['id'],
@@ -300,8 +317,8 @@ class Command(BaseCommand):
             del(card['id'])
         transaction.commit()
 
-        # Associate many-to-many fields
-        self.p('Associating many-to-many fields...\n')
+        # Associate many-to-many fields.
+        self.write('Associating many-to-many fields...\n')
         for card in cards:
             card['db'].super_types.add(*card['super_types'])
             card['db'].card_types.add(*card['card_types'])
@@ -310,16 +327,16 @@ class Command(BaseCommand):
             card['db'].save()
         transaction.commit()
 
-        # Delete old instances
-        self.p('Deleting instances...\n')
+        # Delete old instances.
+        self.write('Deleting instances...\n')
         for card in cards:
             for instance in magic.models.CardInstance.objects.filter(
                     card=card['db']):
                 instance.delete()
         transaction.commit()
 
-        # Create new instances
-        self.p('Creating instances...\n')
+        # Create new instances.
+        self.write('Creating instances...\n')
         for card in cards:
             for set_rarity in card['sets_rarities']:
                 card_instance = magic.models.CardInstance.objects.get_or_create(
@@ -330,4 +347,4 @@ class Command(BaseCommand):
         transaction.commit()
 
         # And we're done!
-        self.p('Done. {0} cards.\n'.format(len(cards)))
+        self.write('Done. {0} cards.\n'.format(len(cards)))
