66ce659627ce4fbecbbc1bfe2d13220f6730c3b7
Taylor Fausak <taylor@fausak.me>
2012-03-06 18:54:51 -0600

    Add card and sub types.

--- magic/admin.py
+++ magic/admin.py
@@ -6,3 +6,15 @@ class SuperTypeAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     search_fields = ['name']
 admin.site.register(magic.models.SuperType, SuperTypeAdmin)
+
+
+class CardTypeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+admin.site.register(magic.models.CardType, CardTypeAdmin)
+
+
+class SubTypeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+admin.site.register(magic.models.SubType, SubTypeAdmin)
--- magic/models.py
+++ magic/models.py
@@ -10,3 +10,25 @@ class SuperType(models.Model):
 
     class Meta:
         ordering = ['name']
+
+
+class CardType(models.Model):
+    name = models.CharField(max_length=12, unique=True)
+    slug = models.SlugField(max_length=12)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class SubType(models.Model):
+    name = models.CharField(max_length=24, unique=True)
+    slug = models.SlugField(max_length=24)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
