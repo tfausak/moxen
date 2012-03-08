a67d78e32be91855bcd9e2e98511c190b25570a4
Taylor Fausak <taylor@fausak.me>
2012-03-07 19:04:22 -0600

    Update liberator shortcut to use logging.

--- liberator/shortcuts.py
+++ liberator/shortcuts.py
@@ -3,9 +3,25 @@
 from liberator.fetch import fetch
 from liberator.parse import parse
 from liberator.store import store
+import logging
 
 
 def liberate(url):
     """Free a card from the clutches of the web!
     """
-    return store(parse(fetch(url)))
+    logging.info('Liberating <{0}>'.format(url))
+
+    fetch_result = fetch(url)
+    if fetch_result is None:
+        logging.warning('Failed to fetch <{0}>'.format(url))
+        return
+
+    parse_result = parse(fetch_result)
+    if parse_result is None:
+        logging.warning('Failed to parse <{0}>'.format(url))
+
+    store_result = store(parse_result)
+    if store_result is None:
+        logging.warning('Failed to store <{0}>'.format(url))
+
+    return store_result
