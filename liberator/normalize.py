ad26154b593b1f998dea426965be7b8c427f107e
Taylor Fausak <taylor@fausak.me>
2012-03-10 00:47:51 -0600

    Add special case for parenthetical names.

--- liberator/normalize.py
+++ liberator/normalize.py
@@ -11,6 +11,13 @@ def normalize(cards):
 def _normalize_card(card):
     """Normalize a single card's fields.
     """
+    # Name
     card['name'] = re.sub('\s+', ' ', card['name'].lower().strip())
+    match = re.search(r'\((.*)\)$', card['name'])
+    if match:
+        card['name'] = match.group(1)
+
+    # Slug
     card['slug'] = slugify(card['name'])
+
     return card
