759ce1007f65389fddb315817bea7e8f0fca9471
Taylor Fausak <taylor@fausak.me>
2012-03-10 00:20:13 -0600

    Remove forms.

--- magic/admin.py
+++ magic/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-import magic.forms
 import magic.models
 
 
 class CardAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
 admin.site.register(magic.models.Card, CardAdmin)
--- magic/models.py
+++ magic/models.py
@@ -4,11 +4,13 @@ from django.db import models
 class Card(models.Model):
     """A Magic: The Gathering card.
     """
+    # Every Magic card has a unique name (201.2). An elemental from
+    # Unhinged has the longest name at 141 characters.
     name = models.CharField(max_length=141, unique=True)
-    slug = models.SlugField(blank=True, max_length=141, unique=True)
-
-    def __unicode__(self):
-        return self.name
+    slug = models.SlugField(max_length=141, unique=True)
 
     class Meta:
         ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
