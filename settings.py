ab66447c7674c26a1223f41255f4dd815f1793c1
Taylor Fausak <taylor@fausak.me>
2012-03-11 01:17:39 -0600

    Rearrange settings.

--- settings.py
+++ settings.py
@@ -1,28 +1,19 @@
+ADMINS = (
+    # ('First Last', 'first.last@example.com'),
+)
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+INTERNAL_IPS = (
+    # '192.168.1.1',
+)
+SECRET_KEY = ''
 
-ROOT_URLCONF = 'bauble.urls'
-SITE_ID = 1
-
+# You probably don't need to edit below this line.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'db.sq3',
     }
 }
-
-TEMPLATE_DIRS = (
-    './templates/',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
@@ -33,4 +24,18 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'liberator',
     'magic',
+)
+MANAGERS = ADMINS
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+ROOT_URLCONF = 'bauble.urls'
+SITE_ID = 1
+TEMPLATE_DEBUG = DEBUG
+TEMPLATE_DIRS = (
+    './templates/',
 )
