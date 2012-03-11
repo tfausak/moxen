bc879ae807f3c9714240458cdfa35cae0c7984a4
Taylor Fausak <taylor@fausak.me>
2012-03-11 16:39:43 -0500

    Add password change functionality.

--- magic/urls.py
+++ magic/urls.py
@@ -1,7 +1,6 @@
 """Django URL configuration.
 """
 from django.conf.urls.defaults import patterns, url
-from django.contrib.auth.views import login, logout
 from magic.views import CardDetailView, CardListView, SearchView
 
 
@@ -11,8 +10,15 @@ urlpatterns = patterns('magic.views',
     url('^card/(?P<slug>[-\w]+)/$', CardDetailView.as_view(),
         name='card_detail'),
     url('^search/$', SearchView.as_view(), name='search'),
-
     url('^accounts/profile/$', 'profile', name='profile'),
-    url('^accounts/login/$', login, name='login'),
-    url('^accounts/logout/$', logout, name='logout'),
+)
+
+
+urlpatterns += patterns('django.contrib.auth.views',
+    url('^accounts/login/$', 'login', name='login'),
+    url('^accounts/logout/$', 'logout', name='logout'),
+    url('^accounts/password/change/$', 'password_change',
+        name='password_change'),
+    url('^accounts/password/change/success/$', 'password_change_done',
+        name='password_change_done'),
 )
