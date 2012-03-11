87974bd290bcced2d7e7353d7369fb02b2d83ca4
Taylor Fausak <taylor@fausak.me>
2012-03-10 22:14:22 -0600

    Add basic card list view.

--- magic/urls.py
+++ magic/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, url
+from magic.views import CardListView
 
 
 urlpatterns = patterns('magic.views',
     url('^$', 'index', name='index'),
+    url('^cards/$', CardListView.as_view(), name='card_list'),
 )
--- magic/views.py
+++ magic/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from magic.models import Card
 
 
 def index(request):
     """TODO
     """
     return render(request, 'magic/index.html')
+
+
+class CardListView(ListView):
+    """TODO
+    """
+    model = Card
+    paginate_by = 100
+
+    def get_context_data(self, **kwargs):
+        context = super(CardListView, self).get_context_data(**kwargs)
+        context['title'] = 'Cards'
+        return context
