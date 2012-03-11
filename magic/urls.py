ab7cbcc1d25a4ed4a47b6d9edc9e860b0a03fcaf
Taylor Fausak <taylor@fausak.me>
2012-03-11 01:10:28 -0600

    Fix regex matching slug in URL.

--- magic/urls.py
+++ magic/urls.py
@@ -5,5 +5,6 @@ from magic.views import CardDetailView, CardListView
 urlpatterns = patterns('magic.views',
     url('^$', 'index', name='index'),
     url('^cards/$', CardListView.as_view(), name='card_list'),
-    url('^card/(?P<slug>.*)/$', CardDetailView.as_view(), name='card_detail'),
+    url('^card/(?P<slug>[-\w]+)/$', CardDetailView.as_view(),
+        name='card_detail'),
 )
