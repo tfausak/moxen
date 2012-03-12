91b119341eb28d50760f0538a926343e297c1a0e
Taylor Fausak <taylor@fausak.me>
2012-03-11 19:53:51 -0500

    Update registration to require unique emails.

--- urls.py
+++ urls.py
@@ -3,12 +3,17 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include
 from django.contrib import admin
+from registration.forms import RegistrationFormUniqueEmail
 
 
 admin.autodiscover()
 urlpatterns = patterns('',
     ('^admin/doc/', include('django.contrib.admindocs.urls')),
     ('^admin/', include(admin.site.urls)),
+    ('^accounts/register/$', 'registration.views.register', {
+            'backend': 'registration.backends.default.DefaultBackend',
+            'form_class': RegistrationFormUniqueEmail,
+        }, 'registration_register'),
     ('^accounts/', include('registration.backends.default.urls')),
     ('', include('magic.urls')),
 )
