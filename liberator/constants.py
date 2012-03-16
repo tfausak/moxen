bd9a741bfad0e503e6fe10e8653ab5e0624655ed
Taylor Fausak <taylor@fausak.me>
2012-03-16 17:03:52 -0500

    Remove obsolete constants.

--- liberator/constants.py
+++ liberator/constants.py
@@ -6,22 +6,9 @@ import re
 GATHERER_URL = re.compile('^http://gatherer[.]wizards[.]com/')
 GATHERER_TEXT_URL = re.compile(r'\bmethod=text\b')
 
-# Every mana cost is made up of zero or more mana symbol.
-MANA_SYMBOL = re.compile(
-    r'({0})|\(({0})/({0})\)'.format(r'\d+|[wubrgxyzpstq]'))
-
-# Each mana symbol has an associated value. It's easier to get these
-# from a dictionary than calling `int()` on everything.
-MANA_COST = defaultdict(lambda: 0, {
-    'w': 1, 'u': 1, 'b': 1, 'r': 1, 'g': 1,
-    '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
-    '10': 10, '11': 11, '12': 12, '13': 13, '14': 14, '15': 15, '16': 16,
-})
-
-# Card normalization depends on knowing which type the card is.
-CREATURE_CARD_TYPE = CardType.objects.get(name='creature')
-PLANESWALKER_CARD_TYPE = CardType.objects.get(name='planeswalker')
-VANGUARD_CARD_TYPE = CardType.objects.get(name='vanguard')
+# Every mana cost is made up of zero or more mana symbols.
+MANA_SYMBOL = r'\d+|[wubrgxyzpstq]'
+MANA_COST = re.compile(r'({0})|\(({0})/({0})\)'.format(MANA_SYMBOL))
 
 # Every super type in the database with a pre-computed regex for
 # matching against the type line.
@@ -46,13 +33,6 @@ for sub_type in SubType.objects.all():
     sub_type.pattern = re.compile(
         r'\b{0}\b'.format(re.escape(sub_type.name)))
     SUB_TYPES.append(sub_type)
-
-# Every color in the database with a pre-computed regex for matching
-# against the mana cost.
-COLORS = []
-for color in Color.objects.all():
-    color.pattern = re.compile(r'\b{0}\b'.format(re.escape(color.slug)))
-    COLORS.append(color)
 
 # Every set in the database with a pre-computed regex for matching
 # against a set-rarity pair.
