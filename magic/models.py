ab927fe1bb37492d2a6da88925d970c33a4f8381
Taylor Fausak <taylor@fausak.me>
2012-03-20 18:33:27 -0500

    Fix #16; add release date to Set model.
    Some sets don't have a release date. At this point, only Vanguard and
    the Promo Set for Gatherer don't have release dates, since they're not
    really sets in the traditional sense.

--- magic/models.py
+++ magic/models.py
@@ -124,6 +124,7 @@ class Set(models.Model):
     """
     name = models.CharField(max_length=39, unique=True)
     slug = models.SlugField(max_length=3, unique=True)
+    release_date = models.DateField(null=True)
 
     class Meta:
         ordering = ['name']
