3a28d80605f7c6e4404decfdff97a4b4d33cff28
Taylor Fausak <taylor@fausak.me>
2012-03-21 10:17:14 -0500

    Make card detail URL consistent with card list.
    In other words, "card" -> "cards".

--- magic/urls.py
+++ magic/urls.py
@@ -6,7 +6,7 @@ from magic.views import CardDetailView, CardListView, SearchView
 urlpatterns = patterns('magic.views',
     url('^$', 'index', name='index'),
     url('^cards/$', CardListView.as_view(), name='card_list'),
-    url('^card/(?P<slug>[-\w]+)/$', CardDetailView.as_view(),
+    url('^cards/(?P<slug>[-\w]+)/$', CardDetailView.as_view(),
         name='card_detail'),
     url('^search/$', SearchView.as_view(), name='search'),
     url('^users/settings/$', 'profile', name='profile'),
