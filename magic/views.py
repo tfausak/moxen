0c7fd6b85a4d452856700d41a8586375081a0566
Taylor Fausak <taylor@fausak.me>
2012-04-03 18:59:27 -0500

    Fix #25; add pagination to printing detail.

--- magic/views.py
+++ magic/views.py
@@ -55,3 +55,22 @@ class PrintingDetailView(DetailView):
     def get_object(self, **_):
         return Printing.objects.get(set__slug=self.kwargs['set_slug'],
             number=self.kwargs['number'], card__slug=self.kwargs['card_slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super(PrintingDetailView, self).get_context_data(**kwargs)
+
+        try:
+            context['previous_printing'] = Printing.objects.get(
+                set__slug=self.kwargs['set_slug'],
+                number=int(self.kwargs['number']) - 1)
+        except Printing.DoesNotExist:
+            context['previous_printing'] = None
+
+        try:
+            context['next_printing'] = Printing.objects.get(
+                set__slug=self.kwargs['set_slug'],
+                number=int(self.kwargs['number']) + 1)
+        except Printing.DoesNotExist:
+            context['next_printing'] = None
+
+        return context
